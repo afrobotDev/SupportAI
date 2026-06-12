2fdf84398472c066f979a195a7077d6ae71468d3
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-12 23:41:25 +0300

    Add dynamic_keys module with MailPreferences type and setPreference function

---
+++ Objects/dynamic_keys.ts
@@ -0,0 +1,15 @@
+export type MailPreferences = {
+  [key: string] : boolean;
+};
+
+
+export function setPreference(
+  preferences: MailPreferences,
+  key: string,
+  value: boolean,
+) {
+  const exists = key in preferences;
+  preferences[key] = value;
+  return exists;
+}
+
