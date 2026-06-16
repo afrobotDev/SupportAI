2fba080114a80065c2b5ee0d1a381e4d7d19910e
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-16 13:54:57 +0300

    Add fn_overloads module

---
+++ Objects/fn_overloads.ts
@@ -0,0 +1,38 @@
+export type MailPreferences = {
+  [key: PropertyKey]: boolean | string;
+  doNotDisturb: boolean;
+  outOfOffice: boolean;
+};
+
+export function configurePreferences(
+  doNotDisturb: boolean,
+  outOfOffice: boolean,
+): MailPreferences;
+
+export function configurePreferences(
+  doNotDisturb: boolean,
+  outOfOffice: boolean | string,
+  useSystemTheme?: boolean,
+  theme?: string,
+): MailPreferences {
+  if (typeof outOfOffice === "string") {
+    return {
+      doNotDisturb: false,
+      outOfOffice: false,
+      useSystemTheme: doNotDisturb,
+      theme: outOfOffice,
+    };
+  } else if (useSystemTheme !== undefined && theme !== undefined) {
+    return {
+      doNotDisturb: doNotDisturb,
+      outOfOffice: outOfOffice,
+      useSystemTheme: useSystemTheme,
+      theme: theme,
+    };
+  } else {
+    return {
+      doNotDisturb: doNotDisturb,
+      outOfOffice: outOfOffice,
+    };
+  }
+}
