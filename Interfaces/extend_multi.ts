cc657c3d02bc4c290211dfdbd632f61d48fb521b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-18 15:25:10 +0300

    Add extend_multi.ts with multiple interface extension

---
+++ Interfaces/extend_multi.ts
@@ -0,0 +1,19 @@
+export interface CanReply {
+  reply(text: string): string;
+}
+
+export interface CanSummarize {
+  summarize(conversation: string[]): string;
+}
+
+export interface CanAct {
+  takeAction(action: string): void;
+}
+
+
+export interface SmartReplyBot extends CanReply, CanSummarize, CanAct {
+  name: string;
+  status: "online" | "offline" | "starting";
+  shutdown(): string; 
+}
+
