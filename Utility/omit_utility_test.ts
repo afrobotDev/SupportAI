367ddb2442ca2cd5e36a94ad2a896a9679a15d8b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-26 14:43:37 +0300

    Add tests for Omit utility type

---
+++ Utility/omit_utility_test.ts
@@ -0,0 +1,52 @@
+import { describe, it, assert, withSubmit } from "./unit_test.ts";
+import type { User } from "./omit_utility.ts";
+import { stripID } from "./omit_utility.ts";
+
+describe("Omit Utility Type", () => {
+  const user: User = {
+    id: "394",
+    name: "Jack",
+    email: "jack@gmail.com",
+    age: 20,
+  };
+  const strippedID = stripID(user);
+
+  const runTests = [
+    {
+      test: "name",
+      expected: true,
+    },
+    {
+      test: "email",
+      expected: true,
+    },
+  ];
+
+  const submitTests = runTests.concat([
+    {
+      test: "age",
+      expected: true,
+    },
+    {
+      test: "id",
+      expected: false,
+    },
+  ]);
+
+  let tests = runTests;
+  if (withSubmit) {
+    tests = submitTests;
+  }
+
+  for (let i = 0; i < tests.length; i++) {
+    const { test, expected } = tests[i];
+    it(`Test ${i}`, () => {
+      assert.strictEqual(test in strippedID, expected);
+    });
+  }
+
+  const numSkipped = submitTests.length - tests.length;
+  if (numSkipped > 0) {
+    console.log(`- Skip: ${numSkipped} test case(s) for submit`);
+  }
+});
